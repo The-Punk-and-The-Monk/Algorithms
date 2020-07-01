fde288780ed3bc7c1e70bae99b3bb118260b0bd4
LinFeng <who.am.i.am.lf@gmail.com>
2020-07-01 13:06:42 +0800

    每日一题-utf8 编码验证

---
+++ 每日一题/393. UTF-8 编码验证.js
@@ -0,0 +1,54 @@
+/**
+ * @param {number[]} data
+ * @return {boolean}
+ * 位操作
+ */
+var validUtf8 = function(data) {
+  let i = 0
+  while(i < data.length){
+      if(validOneByteUtf8(data[i])){
+          i += 1
+          continue
+      }
+      if(i + 1 < data.length && validTwoByteUtf8(data[i], data[i+1])){
+          i += 2
+          continue
+      }
+      if(i + 2 < data.length && validThreeByteUtf8(data[i], data[i+1], data[i+2])){
+          i += 3
+          continue
+      }
+      if(i + 3 < data.length && validFourByteUtf8(data[i], data[i+1], data[i+2], data[i+3])){
+          i += 4
+          continue
+      }
+      return false
+  }
+  return true
+};
+
+
+/**
+ * 这里有个坑
+ * 197 & 224  // 192 
+ * 197 & 224 == 192 // 0 
+ * (197 & 224) == 192 // true
+ * 不知道为啥
+ */
+function validOneByteUtf8(num){
+  return (num & 128) == 0;
+}
+
+function validTwoByteUtf8(num1, num2){
+  return (num1 & 224) == 192 && (num2 & 192) == 128;
+}
+
+function validThreeByteUtf8(num1, num2, num3){
+  return (num1 & 240) == 224 && (num2 & 192) == 128 && (num3 & 192) == 128;
+}
+
+function validFourByteUtf8(num1, num2, num3, num4){
+  return (num1 & 248) == 240 && (num2 & 192) == 128 && (num3 & 192) == 128 && (num4 & 192) == 128;
+}
+
+console.log(validUtf8([197,130,1]))
--- 每日一题/test.js
+++ 每日一题/test.js
@@ -1,28 +1,39 @@
 /**
- * @param {number[]} nums
+ * @param {number[]} nums1
+ * @param {number[]} nums2
  * @return {number}
  */
-var findMin = function(nums) {
-  if(nums.length == 1){
-      return nums[0]
+var findMedianSortedArrays = function(nums1, nums2) {
+  if(nums1.length > nums2.length){
+      [nums1, nums2] = [nums2, nums1]
   }
+  let m = nums1.length
+  let n = nums2.length
   let left = 0
-  let right = nums.length - 1
-
-  if(nums[right] > nums[0]){
-    return nums[0]
-  }
+  let right = m
+  let halfnum = Math.floor((m+n+1)/2)
   while(left <= right){
-      let mid = left + Math.floor((right - left)/ 2)
-      if((mid == 0 || nums[mid - 1] > nums[mid]) && (mid == nums.length-1 || nums[mid] < nums[mid+1])){
-          return nums[mid]
-      }else if(mid == 0 || nums[mid] > nums[0]){
-          left = mid + 1
+      let i = left + Math.floor((right - left)/2)
+      let j = halfnum - i 
+      if((i >= m || nums1[i] >= nums2[j-1]) 
+          && (i <= 0 || nums2[j] >= nums1[i-1])
+      ){
+          let leftMax = nums1[i-1] != undefined ? nums1[i-1] : -Infinity
+          leftMax = nums2[j-1] != undefined ? Math.max(leftMax, nums2[j-1]) : leftMax
+          if((m + n) % 2 == 1){
+              return leftMax
+          }
+
+          let rightMin = nums1[i] != undefined ? nums1[i] : Infinity
+          rightMin = nums2[j] != undefined ? Math.min(rightMin, nums2[j]) : rightMin
+
+          return (leftMax + rightMin) / 2
+      } else if(i < m && nums1[i] < nums2[j-1]){
+          left = i + 1
       }else{
-          right = mid - 1
+          right = i - 1
       }
   }
 };
 
-let nums = [2, 1]
-console.log(findMin(nums))
+console.log(findMedianSortedArrays([0,0], [0,0]))
