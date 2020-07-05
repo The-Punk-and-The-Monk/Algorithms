e27f3925ba61384e2e16a8be95a7abcaeba23d4b
LinFeng <who.am.i.am.lf@gmail.com>
2020-07-05 15:33:14 +0800

    none

--- 每日一题/test.js
+++ 每日一题/test.js
@@ -1,31 +1,73 @@
 /**
- * @param {number} n
- * @return {number}
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
  */
-var numSquares = function(n) {
-  let squareNumsSet = new Set();
-  for(let i = 1; i*i <= n; i++){
-    squareNumsSet.add(i*i);
+class UnionFind {
+  constructor(){
+    this.f = new Map();
   }
 
-  let q = new Set([n])
-  let level = 0
-  while(q.size != 0){
-    level += 1
-    let newQ = new Set()
-    for(let target of q){
-      if(squareNumsSet.has(target)){
-        return level
+  find = (x) => {   // 查找 x 的所归属的集合
+    if(!this.f.has(x)){
+      this.f.set(x, x)
+    }
+    if(this.f.get(x) != x){
+      this.f.set(x, this.find(this.f.get(x)))
+    }
+    return this.f.get(x)
+  }
+
+  union = (x, y) => {  // 合并集合
+    this.f.set(this.find(x), this.find(y))
+  }
+}
+
+var solve = function(board) {
+  if(!board || board.length == 0 || board[0].length == 0){
+    return 
+  }
+
+  let m = board.length, n = board[0].length
+  let dummy = m * n
+  let unionFind = new UnionFind()
+
+  function cntIdx(i, j){
+    return i * n + j
+  }
+
+  for(let i = 0; i < m; i++){
+    for(let j = 0; j < n; j++){
+      if(board[i][j] != 'O'){
+        continue
       }
-      for(let squareNum of squareNumsSet){
-        if(squareNum <= target){
-          newQ.add(target - squareNum)
+      let cur = cntIdx(i, j)
+      if(i == 0 || i == m - 1 || j == 0 || j == n - 1){
+        unionFind.union(cur, dummy)
+      }
+      for(let [x, y] of [[1, 0], [-1, 0], [0, 1], [0, -1]]){
+        let tmpX = i + x, tmpY = j + y
+        if(tmpX >= 0 && tmpX < m && tmpY >= 0 && tmpY < n && board[tmpX][tmpY] == 'O'){
+          unionFind.union(cntIdx(tmpX, tmpY), cur)
         }
       }
     }
-    q = newQ
   }
-  return -1
+
+  for(let i = 0; i < m; i++){
+    for(let j = 0; j < n; j++){
+      if(unionFind.find(cntIdx(i, j)) == unionFind.find(dummy)){
+        board[i][j] = 'O'
+      }else{
+        board[i][j] = 'X'
+      }
+    }
+  }
 };
 
-console.log(numSquares(13))
+let board= [
+  ["X","O","X","O","X","O"],
+  ["O","X","O","X","O","X"],
+  ["X","O","X","O","X","O"],
+  ["O","X","O","X","O","X"]]
+solve(board)
+console.log(board)
