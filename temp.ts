0733994afd93b27ebd04015315437160340e500e
Joey Lin <joeylin@Joeys-MacBook-Pro.local>
2023-05-30 08:58:42 +0800

    update

--- temp.ts
+++ temp.ts
@@ -1,53 +1,32 @@
-function closeStrings(word1: string, word2: string): boolean {
-    if (word1.length !== word2.length) {
-        return false
-    }
-    const { charFrequency: charFrequencyOfWord1, charSet: charSet1 } = getCharFrequencyAscending(word1);
-    const { charFrequency: charFrequencyOfWord2, charSet: charSet2 } = getCharFrequencyAscending(word2);
-    return isEqualArray(charFrequencyOfWord1, charFrequencyOfWord2) && isEqualSet(charSet1, charSet2)
-};
-
-function isEqualArray(arr1: number[], arr2: number[]) {
-    if (arr1.length !== arr2.length) {
-        return false;
-    }
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-            return false
+class NumMatrix {
+    private preSumMatrix: number[][];
+    constructor(matrix: number[][]) {
+        this.preSumMatrix = [];
+        let lastRowOfPreSum: number[] | null = null;
+        for (let i = 0; i < matrix.length; i++) {
+            const currRowOfPreSum: number[] = [];
+            const rowOfMatrix = matrix[i];
+            let preSumOfCurrRowOfMatrix = 0;
+            for (let j = 0; j < rowOfMatrix.length; j++) {
+                preSumOfCurrRowOfMatrix += rowOfMatrix[j];
+                currRowOfPreSum.push(preSumOfCurrRowOfMatrix + (lastRowOfPreSum?.[j] || 0))
+            }
+            lastRowOfPreSum = currRowOfPreSum;
+            this.preSumMatrix.push(currRowOfPreSum)
         }
     }
-    return true
+
+    sumRegion(row1: number, col1: number, row2: number, col2: number): number {
+        const regionSumOfRow1Minus1AndCol1Minus1 = this.preSumMatrix[row1 - 1]?.[col1 - 1] || 0
+        const regionSumOfRow1Minus1AndCol2 = this.preSumMatrix[row1 - 1]?.[col2] || 0
+        const regionSumOfRow2AndCol1Minus1 = this.preSumMatrix[row2]?.[col1 - 1] || 0
+        const regionSumOfRow2AndCol2 = this.preSumMatrix[row2]?.[col2] || 0
+        return regionSumOfRow2AndCol2 - regionSumOfRow1Minus1AndCol2 - regionSumOfRow2AndCol1Minus1 + regionSumOfRow1Minus1AndCol1Minus1
+    }
 }
 
-function isEqualSet(set1: Set<any>, set2: Set<any>) {
-    if (set1.size !== set2.size) {
-        return false
-    }
-    for (let v of set1.values()) {
-        if (!set2.has(v)) {
-            return false
-        }
-    }
-    return true
-}
-
-function getCharFrequencyAscending(word: string): { charFrequency: number[], charSet: Set<string> } {
-    const distribution: Record<string, number> = {};
-    const charFrequencyArray = [];
-    const charSet = new Set<string>();
-    for (let c of word) {
-        if (!distribution[c]) {
-            distribution[c] = 0
-        }
-        distribution[c] += 1
-        charSet.add(c);
-    }
-
-    for (let k of Object.keys(distribution)) {
-        charFrequencyArray.push(distribution[k])
-    }
-    return {
-        charFrequency: charFrequencyArray.sort((a, b) => a - b),
-        charSet
-    };
-}
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
