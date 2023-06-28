9a991a11bdc5b0b1d10cc97c9e45d43d7f361183
Joey <joey@Joey-iMac-Pro.local>
2023-06-28 08:35:06 +0800

    update sliding window

--- sliding-window/readme.md
+++ sliding-window/readme.md
@@ -1,11 +1,84 @@
 # Sliding Window
 
+## Summary
+
+1. The "sliding window" algorithm is suitable for problems where you need to find a contiguous subarray or substring with certain properties, such as the maximum or minimum sum, longest or shortest length, or any other property that can be evaluated within a consecutive range of elements. Common usage scenarios include analyzing time series data, string manipulation and pattern matching problems, and certain optimizations in array problems.
+
+2. In the context of the "sliding window", consider these three questions to ask yourself:
+
+3. When should the window be expanded?
+4. When should the window be reduced?
+5. When should the answer be updated?
+
 ## Reference
+
 1. https://www.geeksforgeeks.org/window-sliding-technique/
 2. https://www.cnblogs.com/huansky/p/13488234.html
 
+## 76. Minimum Window Substring
+
+```
+function minWindow(s: string, t: string): string {
+    const charCountOfT = countChars(t);
+    const charCountOfWindow: Record<string, number> = {};
+    Object.entries(charCountOfT).forEach(([char]) => {
+        charCountOfWindow[char] = 0;
+    })
+    const countOfNonDuplicatedCharOfT = Object.keys(charCountOfT).length;
+    let validCharsCount = 0;
+    let l = 0;
+    let r = 0;
+    let minLen = Infinity;
+    let start = -1;
+
+    while (r < s.length) {
+        while (validCharsCount < countOfNonDuplicatedCharOfT && r < s.length) {
+            const rc = s[r];
+            r++;
+            if (charCountOfT[rc]) {
+                charCountOfWindow[rc] += 1
+                if (charCountOfWindow[rc] === charCountOfT[rc]) {
+                    validCharsCount++;
+                }
+            }
+        }
+
+        while (validCharsCount === countOfNonDuplicatedCharOfT) {
+            const currLen = r - l;
+            if (currLen < minLen) {
+                minLen = currLen
+                start = l;
+            }
+
+            const lc = s[l];
+            l++;
+            if (charCountOfT[lc]) {
+                charCountOfWindow[lc] -= 1;
+                if (charCountOfWindow[lc] < charCountOfT[lc]) {
+                    validCharsCount--;
+                }
+            }
+        }
+    }
+    return start !== -1 ? s.substring(start, start + minLen) : ''
+}
+
+function countChars(s: string): Record<string, number> {
+    const charCount: Record<string, number> = {};
+    for (let c of s) {
+        if (!charCount[c]) {
+            charCount[c] = 0;
+        }
+        charCount[c] += 1;
+    }
+    return charCount;
+}
+```
+
 ## [1004. Max Consecutive Ones III](https://leetcode.com/problems/max-consecutive-ones-iii/description/)
+
 ### typical solution
+
 ```typescript
 function longestOnes(nums: number[], k: number): number {
     let i = 0;
@@ -22,11 +95,13 @@ function longestOnes(nums: number[], k: number): number {
         maximum = Math.max(maximum, j - i + 1);
     }
     return maximum;
-};
+}
 ```
 
 ### ultimate solution
+
 think this as eating beans, and the maximum amount of beans we can eat is k
+
 ```typescript
 function longestOnes(nums: number[], k: number): number {
     let i = 0;
@@ -41,9 +116,11 @@ function longestOnes(nums: number[], k: number): number {
         }
     }
     return j - i;
-};
+}
 ```
 
 ### Similar Questions
 
 #### [1493. Longest Subarray of 1's After Deleting One Element](https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/description/)
+
+3. Longest Substring Without Repeating Characters
--- temp.ts
+++ temp.ts
@@ -1,198 +1,55 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+function minWindow(s: string, t: string): string {
+    const charCountOfT = countChars(t);
+    const charCountOfWindow: Record<string, number> = {};
+    Object.entries(charCountOfT).forEach(([char]) => {
+        charCountOfWindow[char] = 0;
+    });
+    const countOfNonDuplicatedCharOfT = Object.keys(charCountOfT).length;
+    let validCharsCount = 0;
+    let l = 0;
+    let r = 0;
+    let minLen = Infinity;
+    let start = -1;
 
-import { ListNode } from './typical-data-structures/list-node';
+    while (r < s.length) {
+        while (validCharsCount < countOfNonDuplicatedCharOfT && r < s.length) {
+            const rc = s[r];
+            r++;
+            if (charCountOfT[rc]) {
+                charCountOfWindow[rc] += 1;
+                if (charCountOfWindow[rc] === charCountOfT[rc]) {
+                    validCharsCount++;
+                }
+            }
+        }
 
-export enum HeapTypes {
-    MAX,
-    MIN,
-}
+        while (validCharsCount === countOfNonDuplicatedCharOfT) {
+            const currLen = r - l;
+            if (currLen < minLen) {
+                minLen = currLen;
+                start = l;
+            }
 
-export class Heap<T> {
-    public heapType: HeapTypes;
-    private arr: T[];
-    private compare: (item1: T, item2: T) => boolean;
-
-    constructor(params: {
-        arr: T[];
-        heapType: HeapTypes;
-        // return true if item1 big than item2
-        compare: (item1: T, item2: T) => boolean;
-    }) {
-        this.arr = params.arr;
-        this.heapType = params.heapType;
-        this.compare = params.compare;
-        this.initHeap();
-    }
-
-    get length() {
-        return this.arr.length;
-    }
-
-    get top() {
-        return this.arr[0];
-    }
-
-    public pop() {
-        this.swap(0, this.arr.length - 1);
-        const res = this.arr.pop();
-        this.sinkOrSwim(0);
-
-        return res;
-    }
-
-    public add(item: T) {
-        this.arr.push(item);
-        let i = this.arr.length - 1;
-        while (i > 0) {
-            const p = this.parent(i);
-            const isChildBiggerThanParent = this.compare(this.arr[i], this.arr[p]);
-            if (
-                (this.heapType === HeapTypes.MAX && isChildBiggerThanParent) ||
-                (this.heapType === HeapTypes.MIN && !isChildBiggerThanParent)
-            ) {
-                this.swap(i, p);
-                i = p;
-            } else {
-                break;
+            const lc = s[l];
+            l++;
+            if (charCountOfT[lc]) {
+                charCountOfWindow[lc] -= 1;
+                if (charCountOfWindow[lc] < charCountOfT[lc]) {
+                    validCharsCount--;
+                }
             }
         }
     }
-
-    private initHeap() {
-        for (let i = this.parent(this.arr.length - 1); i >= 0; i--) {
-            this.sinkOrSwim(i);
-        }
-    }
-
-    private sinkOrSwim(index: number) {
-        if (this.heapType === HeapTypes.MAX) {
-            return this.biggestGoesUp(index);
-        }
-        return this.smallestGoesUp(index);
-    }
-
-    /**
-     * smallest goes up
-     * bigger ones smallestGoesUp down
-     * @param index
-     * @returns
-     */
-    private smallestGoesUp(index: number) {
-        if (index >= this.arr.length) {
-            return;
-        }
-        const leftChildIndex = this.leftChild(index);
-        const rightChildIndex = this.rightChild(index);
-        const currItem = this.arr[index];
-        const leftChild = this.arr[leftChildIndex];
-        const rightChild = this.arr[rightChildIndex];
-        const isLeftChildExist = leftChild !== undefined;
-        const isRightChildExist = rightChild !== undefined;
-        const isParentBiggerThanLeftChild = isLeftChildExist ? this.compare(currItem, leftChild) : false;
-        const isParentBiggerThanRightChild = isRightChildExist ? this.compare(currItem, rightChild) : false;
-        const isLeftChildBiggerThanRightChild =
-            isLeftChildExist && isRightChildExist ? this.compare(leftChild, rightChild) : false;
-        if (!(isParentBiggerThanLeftChild || isParentBiggerThanRightChild)) {
-            return;
-        }
-        if (isLeftChildBiggerThanRightChild && rightChildIndex < this.arr.length) {
-            this.swap(index, rightChildIndex);
-            this.smallestGoesUp(rightChildIndex);
-            return;
-        }
-        if (!isLeftChildBiggerThanRightChild && leftChildIndex < this.arr.length) {
-            this.swap(index, leftChildIndex);
-            this.smallestGoesUp(leftChildIndex);
-        }
-    }
-
-    /**
-     * biggest biggestGoesUp up
-     * smaller ones go down
-     * @param index
-     * @returns
-     */
-    private biggestGoesUp(index: number) {
-        if (index >= this.arr.length) {
-            return;
-        }
-        const leftChildIndex = this.leftChild(index);
-        const rightChildIndex = this.rightChild(index);
-        const parent = this.arr[index];
-        const leftChild = this.arr[leftChildIndex];
-        const rightChild = this.arr[rightChildIndex];
-        const isLeftChildExist = leftChild !== undefined;
-        const isRightChildExist = rightChild !== undefined;
-        const isParentSmallerThanLeftChild = isLeftChildExist ? this.compare(leftChild, parent) : false;
-        const isParentSmallerThanRightChild = isRightChildExist ? this.compare(rightChild, parent) : false;
-        const isLeftChildSmallerThanRightChild =
-            isLeftChildExist && isRightChildExist ? this.compare(rightChild, leftChild) : false;
-        if (!(isParentSmallerThanLeftChild || isParentSmallerThanRightChild)) {
-            return;
-        }
-        if (isLeftChildSmallerThanRightChild && rightChildIndex < this.arr.length) {
-            this.swap(index, rightChildIndex);
-            this.biggestGoesUp(rightChildIndex);
-            return;
-        }
-        if (!isLeftChildSmallerThanRightChild && leftChildIndex < this.arr.length) {
-            this.swap(index, leftChildIndex);
-            this.biggestGoesUp(leftChildIndex);
-        }
-    }
-
-    private swap(index1: number, index2: number) {
-        const temp = this.arr[index1];
-        this.arr[index1] = this.arr[index2];
-        this.arr[index2] = temp;
-    }
-
-    private parent(index: number) {
-        return Math.ceil(index / 2) - 1;
-    }
-
-    private leftChild(index: number) {
-        return index * 2 + 1;
-    }
-
-    private rightChild(index: number) {
-        return index * 2 + 2;
-    }
+    return start !== -1 ? s.substring(start, start + minLen) : '';
 }
 
-function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
-    lists = lists.filter((node) => !!node);
-    const dummyNode = new ListNode(-1);
-    const heap = new Heap<ListNode>({
-        arr: lists as Array<ListNode>,
-        heapType: HeapTypes.MIN,
-        compare: (node1, node2) => node1.val > node2.val,
-    });
-
-    let tail = dummyNode;
-
-    while (heap.length) {
-        const p = heap.pop();
-        if (!p) {
-            continue;
+function countChars(s: string): Record<string, number> {
+    const charCount: Record<string, number> = {};
+    for (let c of s) {
+        if (!charCount[c]) {
+            charCount[c] = 0;
         }
-        const next = p.next;
-        p.next = null;
-        tail.next = p;
-        tail = tail.next;
-        if (next) {
-            heap.add(next);
-        }
+        charCount[c] += 1;
     }
-    return dummyNode.next;
+    return charCount;
 }
