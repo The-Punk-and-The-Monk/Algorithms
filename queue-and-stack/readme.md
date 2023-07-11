12fd6f28a2382a92e5276eb2503ea6a0b7587a53
Joey <joey@Joey-iMac-Pro.local>
2023-07-11 21:25:16 +0800

    monotonic stack && queue

--- queue-and-stack/readme.md
+++ queue-and-stack/readme.md
@@ -11,3 +11,73 @@
 A monotone stack is really just a stack, with some clever logic that keeps the elements in order (monotonically increasing or monotonically decreasing) every time a new element is added to the stack. Sounds like a heap, right? No, monotone stacks are less useful and only deal with a typical class of problems, such as "next larger element", "last smaller element", etc.
 
 496. Next Greater Element I 496. 下⼀个更⼤元素 I 🟢 503. Next Greater Element II 503. 下⼀个更⼤元素 II 🟠 739. Daily Temperatures 739. 每⽇温度 🟠 - 剑指 Offer II 038. 每⽇温度
+
+```typescript
+function nextGreaterElements(nums: number[]): number[] {
+    if (!nums?.length) {
+        return [];
+    }
+    const res = new Array(nums.length).fill(-1);
+    const stack = [];
+    const n = nums.length;
+    let i = nums.length * 2 - 1;
+    while (i >= 0) {
+        while (stack.length && stack[stack.length - 1] <= nums[i % n]) {
+            stack.pop();
+        }
+        res[i % n] = stack.length ? stack[stack.length - 1] : -1;
+        stack.push(nums[i % n]);
+        i--;
+    }
+    return res;
+}
+```
+
+## Monotonic Queue
+
+给你⼀个数组 window，已知其最值为 A，如果给 window 中添加⼀个数 B，那么⽐较⼀下 A 和 B 就可以⽴即 算出新的最值；但如果要从 window 数组中减少⼀个数，就不能直接得到最值了，因为如果减少的这个数恰 好是 A，就需要遍历 window 中的所有元素重新寻找新的最值
+
+239. Sliding Window Maximum
+
+```typescript
+class MonotonicQueue {
+    private arr: number[];
+    constructor() {
+        this.arr = [];
+    }
+    public pop(n: number) {
+        if (!this.arr.length || this.arr[0] !== n) {
+            return;
+        }
+        return this.arr.shift();
+    }
+
+    public push(n: number) {
+        while (this.arr.length && this.arr[this.arr.length - 1] < n) {
+            this.arr.pop();
+        }
+        this.arr.push(n);
+    }
+    public max() {
+        return this.arr[0];
+    }
+}
+
+function maxSlidingWindow(nums: number[], k: number): number[] {
+    if (!nums.length || k <= 0) {
+        return [];
+    }
+    const queue = new MonotonicQueue();
+    const res: number[] = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (i < k - 1) {
+            queue.push(nums[i]);
+        } else {
+            queue.push(nums[i]);
+            res.push(queue.max());
+            queue.pop(nums[i - k + 1]);
+        }
+    }
+    return res;
+}
+```
--- temp.ts
+++ temp.ts
@@ -1,30 +1,143 @@
-function advantageCount(nums1: number[], nums2: number[]): number[] {
-    if (!nums1?.length || !nums2?.length || nums1.length !== nums2.length) {
-        return nums1;
+interface HeapItem {
+    value: number;
+    heapIndex: number;
+}
+
+class MonotonicQueue {
+    private arr: HeapItem[];
+    private queue: HeapItem[];
+
+    constructor() {
+        this.arr = [];
+        this.queue = [];
+        this.initHeap();
     }
-    const nums2WithIndex: { v: number; i: number }[] = [];
-    nums2.forEach((v, i) => {
-        nums2WithIndex.push({
-            v,
-            i,
-        });
-    });
-    nums2WithIndex.sort((a, b) => b.v - a.v);
-    nums1.sort((a, b) => a - b);
-    let l = 0;
-    let r = nums1.length - 1;
-    const res = new Array(nums1.length).fill(-1);
-    for (let h of nums2WithIndex) {
-        const { v, i } = h;
-        if (nums1[r] > v) {
-            res[i] = nums1[r];
-            r--;
+
+    public push(n: number) {
+        this.addToHeap(n);
+    }
+
+    public pop() {
+        const targetItem = this.queue.pop();
+        if (!targetItem) {
+            return;
+        }
+        this.removeFromHeap(targetItem?.heapIndex);
+    }
+
+    public max() {
+        return this.arr[0].value;
+    }
+
+    private removeFromHeap(i: number) {
+        this.swap(i, this.arr.length - 1);
+        this.arr.pop();
+        this.sinkOrSwim(0);
+    }
+
+    private addToHeap(n: number) {
+        const item = { value: n, heapIndex: this.arr.length };
+        this.arr.push(item);
+        this.queue.push(item);
+        let i = this.arr.length - 1;
+        while (i > 0) {
+            const p = this.parent(i);
+
+            const isChildBiggerThanParent = !!(this.arr[i].value - this.arr[p].value);
+            if (isChildBiggerThanParent) {
+                this.swap(i, p);
+                i = p;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private initHeap() {
+        for (let i = this.parent(this.arr.length - 1); i >= 0; i--) {
+            this.sinkOrSwim(i);
+        }
+    }
+
+    private sinkOrSwim(index: number) {
+        return this.biggestGoesUp(index);
+    }
+
+    private compare(i: HeapItem, j: HeapItem) {
+        return !!(i.value - j.value);
+    }
+
+    /**
+     * biggest biggestGoesUp up
+     * smaller ones go down
+     * @param index
+     * @returns
+     */
+    private biggestGoesUp(index: number) {
+        if (index >= this.arr.length) {
+            return;
+        }
+        const leftChildIndex = this.leftChild(index);
+        const rightChildIndex = this.rightChild(index);
+        const parent = this.arr[index];
+        const leftChild = this.arr[leftChildIndex];
+        const rightChild = this.arr[rightChildIndex];
+        const isLeftChildExist = leftChild !== undefined;
+        const isRightChildExist = rightChild !== undefined;
+        const isParentSmallerThanLeftChild = isLeftChildExist ? this.compare(leftChild, parent) : false;
+        const isParentSmallerThanRightChild = isRightChildExist ? this.compare(rightChild, parent) : false;
+        const isLeftChildSmallerThanRightChild =
+            isLeftChildExist && isRightChildExist ? this.compare(rightChild, leftChild) : false;
+        if (!(isParentSmallerThanLeftChild || isParentSmallerThanRightChild)) {
+            return;
+        }
+        if (isLeftChildSmallerThanRightChild && rightChildIndex < this.arr.length) {
+            this.swap(index, rightChildIndex);
+            this.biggestGoesUp(rightChildIndex);
+            return;
+        }
+        if (!isLeftChildSmallerThanRightChild && leftChildIndex < this.arr.length) {
+            this.swap(index, leftChildIndex);
+            this.biggestGoesUp(leftChildIndex);
+        }
+    }
+
+    private swap(index1: number, index2: number) {
+        const temp = this.arr[index1];
+        this.arr[index1] = this.arr[index2];
+        this.arr[index2] = temp;
+
+        this.arr[index1].heapIndex = index1;
+        this.arr[index2].heapIndex = index2;
+    }
+
+    private parent(index: number) {
+        return Math.ceil(index / 2) - 1;
+    }
+
+    private leftChild(index: number) {
+        return index * 2 + 1;
+    }
+
+    private rightChild(index: number) {
+        return index * 2 + 2;
+    }
+}
+
+function maxSlidingWindow(nums: number[], k: number): number[] {
+    const res: number[] = [];
+    if (!nums?.length || k <= 0) {
+        return res;
+    }
+    const queue = new MonotonicQueue();
+    for (let i = 0; i < nums.length; i++) {
+        if (i < k - 1) {
+            queue.push(nums[i]);
         } else {
-            res[i] = nums1[l];
-            l++;
+            queue.push(nums[i]);
+            res.push(queue.max());
+            queue.pop();
         }
     }
     return res;
 }
-
-console.log(advantageCount([12, 24, 8, 32], [13, 25, 32, 11]));
